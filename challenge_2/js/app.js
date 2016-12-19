79d2a13e8e75da9a854defc3a0fac2eff9becd22
briandridge <briandridge@gmail.com>
2016-12-19 13:03:33 -0700

    challenge 2 without methods

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,22 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("ClassController", ClassController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Brian Ridge",
+  this.age = "30",
+  this.city = "Golden",
+  this.state = "Colorado"
+}
+
+function ClassController() {
+	this.class_name = "WDI",
+	this.enrolled_students = "me",
+	this.start_date = "Halloween",
+	this.end_date = "Feb 2nd"
+	this.daysRemaining = function() {
+		
+	};
+}
